1bf5aec4eb6950b309a7f0967bf6127fbe6058d1
alex sanchez <alexjsanchez1020@gmail.com>
2022-12-20 09:05:42 -0500

    add cars seed

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,29 @@
 // STRETCH
+const cars = [
+    {
+        vin:123456789123,
+        make: 'nissan',
+        model: 'prius',
+        mileage: 205000,
+        transmission: 'manual',
+    },
+    {
+        vin:123456789123,
+        make: 'toyota',
+        model: 'corolla',
+        mileage: 205000,
+        transmission: 'manual',
+    },
+    {
+        vin:123456789123,
+        make: 'ford',
+        model: 'focus',
+        mileage: 205000,
+        transmission: 'manual',
+    }
+]
+
+exports.seed = async function(knex){
+   await knex('cars').truncate()
+   await knex('cars').insert(cars)
+}
